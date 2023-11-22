b587488128e503b887ae1fdd1b27003f4fc90abd
Jose Alvarez <jealv@icloud.com>
2023-11-21 22:40:51 -0500

    WIP commit

--- main.ts
+++ main.ts
@@ -1,134 +1,168 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { detailedDiff } from "deep-object-diff";
+import {
+	FrontMatterCache,
+	Notice,
+	Plugin,
+	TFile,
+	normalizePath,
+} from "obsidian";
 
 interface MyPluginSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	frontmatterCache: Record<string, FrontMatterCache> = {};
+
+	async isTemplate(file: TFile) {
+		let templatesConfig: string;
+		try {
+			templatesConfig = await this.app.vault.adapter.read(
+				normalizePath(this.app.vault.configDir + "/templates.json")
+			);
+		} catch {
+			throw new Error(
+				"Failed to read templates config. Make sure the core plugin is enabled."
+			);
+		}
+
+		let templatesFolder: string;
+		try {
+			templatesFolder = JSON.parse(templatesConfig).folder;
+		} catch {
+			throw new Error(
+				"Failed to get templates folder from templates config. Make sure you've configured a template folder location in the core plugin's settings."
+			);
+		}
+		return file.path.startsWith(templatesFolder);
+	}
+
+	applyDiff(obj: any, diff: any) {
+		const result = obj;
+
+		const applyRecursively = (target: any, change: any) => {
+			for (const key in change) {
+				if (change[key] && typeof change[key] === "object") {
+					if (!target[key]) {
+						target[key] = Array.isArray(change[key]) ? [] : {};
+					}
+					applyRecursively(target[key], change[key]);
+				} else {
+					if (change[key] === null) {
+						delete target[key];
+					} else {
+						target[key] = change[key];
+					}
+				}
+			}
+		};
+
+		applyRecursively(result, diff.added);
+		applyRecursively(result, diff.updated);
+		applyRecursively(result, diff.deleted);
+
+		return result;
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.app.workspace.onLayoutReady(() => {
+			// vault events happen before metadata cache events,
+			// so we listen so that we can diff the old and new frontmatter
+			this.app.vault.on("modify", async (file) => {
+				if (!(file instanceof TFile)) return;
+				if (!(await this.isTemplate(file))) return;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+				const frontmatter =
+					this.app.metadataCache.getFileCache(file)?.frontmatter;
+				if (!frontmatter) return;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				this.frontmatterCache[file.path] = frontmatter;
+			});
+			this.app.vault.on("rename", async (file, oldPath) => {
+				delete this.frontmatterCache[oldPath];
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (!(file instanceof TFile)) return;
+				if (!(await this.isTemplate(file))) return;
+
+				const cache = this.app.metadataCache.getFileCache(file);
+				if (!cache?.frontmatter) return;
+				this.frontmatterCache[file.path] = cache.frontmatter;
+			});
+			this.app.vault.on("delete", async (file) => {
+				delete this.frontmatterCache[file.path];
+			});
+
+			this.app.metadataCache.on("changed", async (file) => {
+				if (!(file instanceof TFile)) return;
+				if (!(await this.isTemplate(file))) return;
+
+				const oldFrontmatter = this.frontmatterCache[file.path];
+				const newFrontmatter =
+					this.app.metadataCache.getFileCache(file)?.frontmatter;
+				if (!(oldFrontmatter && newFrontmatter)) return;
+
+				const diff = detailedDiff(oldFrontmatter, newFrontmatter);
+				console.log(diff);
+				if (!diff) return;
+
+				// use undocumented API to get the template's backlinks
+				const linkedFiles = Object.keys(
+					// @ts-ignore
+					this.app.metadataCache.getBacklinksForFile(file).data
+				)
+					.map((link) => this.app.vault.getAbstractFileByPath(link))
+					.filter((file): file is TFile => file instanceof TFile);
+				if (!linkedFiles.length) return;
+
+				// at this point, we know these files have a backlink to the template
+				// but we don't know that that link is in the frontmatter template field
+				let updateCount = 0;
+				for await (const linkedFile of linkedFiles) {
+					const cache =
+						this.app.metadataCache.getFileCache(linkedFile);
+					if (!cache) continue;
+
+					const hasLink = cache.frontmatterLinks?.some(
+						(link) =>
+							link.key === "template" &&
+							this.app.metadataCache.getFirstLinkpathDest(
+								link.link,
+								linkedFile.path
+							) === file
+					);
+					if (!hasLink) continue;
+
+					await this.app.fileManager.processFrontMatter(
+						linkedFile,
+						(frontmatter) => this.applyDiff(frontmatter, diff)
+					);
+					updateCount++;
 				}
-			}
+
+				if (!updateCount) return;
+				new Notice(
+					`Updated ${updateCount} file(s) linked to ${file.basename}`
+				);
+			});
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
